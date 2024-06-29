6e0d8b7105e484985fe7cd808eb6a5df61fff3fa
Nativel <nativelbk@gmail.com>
2024-06-29 12:15:18 +0300

    add global connection

---
+++ lib/mongoose.ts
@@ -0,0 +1,52 @@
+/** @format */
+
+import mongoose, { Mongoose } from "mongoose";
+
+const MONGODB_URI: string | undefined = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
+}
+
+interface MongooseCache {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+/**
+ * Global is used here to maintain a cached connection
+ * across hot reloads in development. This prevents
+ * connections growing exponentially during API Route usage.
+ */
+declare global {
+  // eslint-disable-next-line no-var
+  var mongoose: MongooseCache;
+}
+
+let cached: MongooseCache = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect(): Promise<Mongoose> {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI!, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default dbConnect;
