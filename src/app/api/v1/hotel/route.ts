505da9b153f18a9d0b55809f4845b5b0edcaf502
Nativel <nativelbk@gmail.com>
2024-06-29 19:04:43 +0300

    add crud operation to hotel

--- src/app/api/v1/hotel/route.ts
+++ src/app/api/v1/hotel/route.ts
@@ -5,3 +5,10 @@ import Hotel from "@/models/Hotel";
 export function POST() {
   return "create hotel";
 }
+
+export function DELETE() {
+  return "delete hotel";
+}
+export function GET() {
+  return "get hotels";
+}
