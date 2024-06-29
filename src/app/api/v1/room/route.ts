8d36418f9d5c3a8d7b2dde7e37a40ee94bbfc31c
Nativel <nativelbk@gmail.com>
2024-06-29 20:24:31 +0300

    update /room route

--- src/app/api/v1/room/route.ts
+++ src/app/api/v1/room/route.ts
@@ -1,19 +1,63 @@
 /** @format */
-
+import { NextApiRequest } from "next";
+import { NextResponse } from "next/server";
+import dbConnect from "@/lib/mongoose";
 import Room from "@/models/Room";
+import Joi from "joi";
 
-export function POST() {
-  return "create room";
-}
+const roomSchema = Joi.object({
+  hotel: Joi.string().hex().length(24).required(),
+  number: Joi.string().required(),
+  type: Joi.string().required(),
+  price: Joi.number().required(),
+  description: Joi.string(),
+  available: Joi.boolean(),
+  roomImages: Joi.array().items(Joi.string()),
+});
 
-export function DELETE() {
-  return "delete room";
-}
+export const POST = async (req: NextApiRequest) => {
+  await dbConnect();
 
-export function GET() {
-  return "get rooms";
-}
+  const { error } = roomSchema.validate(req.body);
+  if (error) {
+    return NextResponse.json(
+      { success: false, error: error.details[0].message },
+      { status: 400 }
+    );
+  }
 
-export function PATCH() {
-  return "get room";
-}
+  try {
+    const { hotel, number, type, price, description, available, roomImages } =
+      req.body;
+    const newRoom = new Room({
+      hotel,
+      number,
+      type,
+      price,
+      description,
+      available,
+      roomImages,
+    });
+    await newRoom.save();
+    return NextResponse.json({ success: true, data: newRoom }, { status: 201 });
+  } catch (error: any) {
+    return NextResponse.json(
+      { success: false, error: error.message },
+      { status: 400 }
+    );
+  }
+};
+
+export const GET = async () => {
+  await dbConnect();
+
+  try {
+    const rooms = await Room.find({});
+    return NextResponse.json({ success: true, data: rooms });
+  } catch (error: any) {
+    return NextResponse.json(
+      { success: false, error: error.message },
+      { status: 400 }
+    );
+  }
+};
