eccd23dd87fb5cffbb4368449015249e0f18cffb
Nativel <nativelbk@gmail.com>
2024-06-29 17:51:20 +0300

    add models

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,12 +1,14 @@
 /** @format */
 
 import mongoose, { Schema, Document, Model } from "mongoose";
+import bcrypt from "bcrypt";
 
 interface IUser extends Document {
   username: string;
   email: string;
   password: string;
   role: "customer" | "admin";
+  comparePassword(candidatePassword: string): Promise<boolean>;
 }
 
 const UserSchema: Schema<IUser> = new Schema({
@@ -15,6 +17,18 @@ const UserSchema: Schema<IUser> = new Schema({
   password: { type: String, required: true },
   role: { type: String, enum: ["customer", "admin"], default: "customer" },
 });
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+UserSchema.methods.comparePassword = async function (
+  candidatePassword: string
+) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
 
 const User: Model<IUser> =
   mongoose.models.User || mongoose.model<IUser>("User", UserSchema);
